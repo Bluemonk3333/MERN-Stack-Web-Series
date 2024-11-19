c46503770031f6e31be054140f759f5d6c33beff
ravikumar <ravikumar8955515869@gmail.com>
2024-11-19 13:02:03 +0530

    modification

--- routers/router.js
+++ routers/router.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const router = express();
 
-// create Home Page!
+// for error page simple msg
+const onError = {
+  msg: "Page not found!",
+};
+
+// Home Page!
 router.get("/", (req, resp) => {
   try {
     resp.status(200).json({ msg: "Home Page!" });
   } catch (error) {
-    resp.status(404).json({ msg: "Page not found!" });
+    resp.status(404).json(onError.msg);
+  }
+});
+// About us page!
+router.get("/about", (req, resp) => {
+  try {
+    resp.status(200).json({ msg: "About us Page!" });
+  } catch (error) {
+    resp.status(404).json(onError.msg);
+  }
+});
+// Blog Page
+router.get("/blog", (req, resp) => {
+  try {
+    resp.status(200).json({ msg: "Blog Page!" });
+  } catch (error) {
+    resp.status(404).json(onError.msg);
+  }
+});
+// Contact page!
+router.get("/contact", (req, resp) => {
+  try {
+    resp.status(200).json({ msg: "Contact Page!" });
+  } catch (error) {
+    resp.status(404).json(onError.msg);
   }
 });
 
